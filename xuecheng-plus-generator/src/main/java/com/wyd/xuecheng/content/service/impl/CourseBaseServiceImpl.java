a9e665e38716d399cfc5fe1c83ca5bdff1ad6028
Stone-WYD <1174872613@qq.com>
2024-01-31 17:18:30 +0800

    完成内容模块的初始化和model内容-4

---
+++ xuecheng-plus-generator/src/main/java/com/wyd/xuecheng/content/service/impl/CourseBaseServiceImpl.java
@@ -0,0 +1,21 @@
+package com.wyd.xuecheng.content.service.impl;
+
+import com.wyd.xuecheng.content.mapper.CourseBaseMapper;
+import com.wyd.xuecheng.content.model.po.CourseBase;
+import com.wyd.xuecheng.content.service.CourseBaseService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 课程基本信息 服务实现类
+ * </p>
+ *
+ * @author itcast
+ */
+@Slf4j
+@Service
+public class CourseBaseServiceImpl extends ServiceImpl<CourseBaseMapper, CourseBase> implements CourseBaseService {
+
+}
---
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/content/service/impl/CourseAuditServiceImpl.java
@@ -0,0 +1,21 @@
+package com.xuecheng.content.service.impl;
+
+import com.xuecheng.content.model.po.CourseAudit;
+import com.xuecheng.content.mapper.CourseAuditMapper;
+import com.xuecheng.content.service.CourseAuditService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author wyd
+ */
+@Slf4j
+@Service
+public class CourseAuditServiceImpl extends ServiceImpl<CourseAuditMapper, CourseAudit> implements CourseAuditService {
+
+}
