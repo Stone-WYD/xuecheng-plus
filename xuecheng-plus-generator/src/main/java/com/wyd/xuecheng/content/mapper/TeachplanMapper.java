455d48fe07ede5719ba27820722415b04d3a459d
Stone-WYD <1174872613@qq.com>
2024-01-31 17:17:56 +0800

    完成内容模块的初始化和model内容-3

---
+++ xuecheng-plus-generator/src/main/java/com/wyd/xuecheng/content/mapper/TeachplanMapper.java
@@ -0,0 +1,15 @@
+package com.wyd.xuecheng.content.mapper;
+
+import com.wyd.xuecheng.content.model.po.Teachplan;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 课程计划 Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface TeachplanMapper extends BaseMapper<Teachplan> {
+
+}
---
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/content/service/impl/MqMessageHistoryServiceImpl.java
@@ -0,0 +1,21 @@
+package com.xuecheng.content.service.impl;
+
+import com.xuecheng.content.model.po.MqMessageHistory;
+import com.xuecheng.content.mapper.MqMessageHistoryMapper;
+import com.xuecheng.content.service.MqMessageHistoryService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author wyd
+ */
+@Slf4j
+@Service
+public class MqMessageHistoryServiceImpl extends ServiceImpl<MqMessageHistoryMapper, MqMessageHistory> implements MqMessageHistoryService {
+
+}
