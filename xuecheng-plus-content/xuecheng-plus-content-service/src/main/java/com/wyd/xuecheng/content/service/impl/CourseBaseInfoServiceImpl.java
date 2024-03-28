ec8b861e5355c1d6f60f052f9b624042d63484e7
Stone-WYD <1174872613@qq.com>
2024-03-28 19:22:44 +0800

    新增课程

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/wyd/xuecheng/content/service/impl/CourseBaseInfoServiceImpl.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/wyd/xuecheng/content/service/impl/CourseBaseInfoServiceImpl.java
@@ -3,10 +3,14 @@ package com.wyd.xuecheng.content.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wyd.xuecheng.content.mapper.CourseBaseMapper;
+import com.wyd.xuecheng.content.mapper.CourseCategoryMapper;
+import com.wyd.xuecheng.content.mapper.CourseMarketMapper;
 import com.wyd.xuecheng.content.model.dto.AddCourseDto;
 import com.wyd.xuecheng.content.model.dto.CourseBaseInfoDto;
 import com.wyd.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.wyd.xuecheng.content.model.po.CourseBase;
+import com.wyd.xuecheng.content.model.po.CourseCategory;
+import com.wyd.xuecheng.content.model.po.CourseMarket;
 import com.wyd.xuecheng.content.service.CourseBaseInfoService;
 import com.wyd.xuecheng.model.PageParams;
 import com.wyd.xuecheng.model.PageResult;
@@ -30,7 +34,13 @@ public class CourseBaseInfoServiceImpl  implements CourseBaseInfoService {
 
 
     @Autowired
-    CourseBaseMapper courseBaseMapper;
+    private CourseBaseMapper courseBaseMapper;
+
+    @Autowired
+    private CourseMarketMapper courseMarketMapper;
+
+    @Autowired
+    private CourseCategoryMapper courseCategoryMapper;
 
     @Override
     public PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto) {
@@ -108,10 +118,63 @@ public class CourseBaseInfoServiceImpl  implements CourseBaseInfoService {
         if (insert <= 0) {
             throw new RuntimeException("新增课程基本信息失败");
         }
-        //todo:向课程营销表保存课程营销信息
-        //todo:查询课程基本信息及营销信息并返回
-        return null;
+        //向课程营销表保存课程营销信息
+        //课程营销信息
+        CourseMarket courseMarketNew = new CourseMarket();
+        Long courseId = courseBaseNew.getId();
+        BeanUtils.copyProperties(dto,courseMarketNew);
+        courseMarketNew.setId(courseId);
+        int i = saveCourseMarket(courseMarketNew);
+        if(i<=0){
+            throw new RuntimeException("保存课程营销信息失败");
+        }
+        //查询课程基本信息及营销信息并返回
+        return getCourseBaseInfo(courseId);
     }
 
+    //保存课程营销信息
+    private int saveCourseMarket(CourseMarket courseMarketNew){
+        //收费规则
+        String charge = courseMarketNew.getCharge();
+        if(StringUtils.isBlank(charge)){
+            throw new RuntimeException("收费规则没有选择");
+        }
+        //收费规则为收费
+        if(charge.equals("201001")){
+            if(courseMarketNew.getPrice() == null || courseMarketNew.getPrice() <=0){
+                throw new RuntimeException("课程为收费价格不能为空且必须大于0");
+            }
+        }
+        //根据id从课程营销表查询
+        CourseMarket courseMarketObj = courseMarketMapper.selectById(courseMarketNew.getId());
+        if(courseMarketObj == null){
+            return courseMarketMapper.insert(courseMarketNew);
+        }else{
+            BeanUtils.copyProperties(courseMarketNew,courseMarketObj);
+            courseMarketObj.setId(courseMarketNew.getId());
+            return courseMarketMapper.updateById(courseMarketObj);
+        }
+    }
+    //根据课程id查询课程基本信息，包括基本信息和营销信息
+    public CourseBaseInfoDto getCourseBaseInfo(long courseId){
+
+        CourseBase courseBase = courseBaseMapper.selectById(courseId);
+        if(courseBase == null){
+            return null;
+        }
+        CourseMarket courseMarket = courseMarketMapper.selectById(courseId);
+        CourseBaseInfoDto courseBaseInfoDto = new CourseBaseInfoDto();
+        BeanUtils.copyProperties(courseBase,courseBaseInfoDto);
+        if(courseMarket != null){
+            BeanUtils.copyProperties(courseMarket,courseBaseInfoDto);
+        }
+        //查询分类名称
+        CourseCategory courseCategoryBySt = courseCategoryMapper.selectById(courseBase.getSt());
+        courseBaseInfoDto.setStName(courseCategoryBySt.getName());
+        CourseCategory courseCategoryByMt = courseCategoryMapper.selectById(courseBase.getMt());
+        courseBaseInfoDto.setMtName(courseCategoryByMt.getName());
+
+        return courseBaseInfoDto;
+    }
 
 }
