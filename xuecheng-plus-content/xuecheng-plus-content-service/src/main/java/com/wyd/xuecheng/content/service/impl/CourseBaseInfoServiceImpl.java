bc7a0018020e69f7d5abc170be4b7cf808f40caf
Stone-WYD <1174872613@qq.com>
2024-03-28 21:18:29 +0800

    测试统一异常处理

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/wyd/xuecheng/content/service/impl/CourseBaseInfoServiceImpl.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/wyd/xuecheng/content/service/impl/CourseBaseInfoServiceImpl.java
@@ -2,6 +2,9 @@ package com.wyd.xuecheng.content.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.wyd.xuecheng.base.exception.XueChengPlusException;
+import com.wyd.xuecheng.base.model.PageParams;
+import com.wyd.xuecheng.base.model.PageResult;
 import com.wyd.xuecheng.content.mapper.CourseBaseMapper;
 import com.wyd.xuecheng.content.mapper.CourseCategoryMapper;
 import com.wyd.xuecheng.content.mapper.CourseMarketMapper;
@@ -12,8 +15,6 @@ import com.wyd.xuecheng.content.model.po.CourseBase;
 import com.wyd.xuecheng.content.model.po.CourseCategory;
 import com.wyd.xuecheng.content.model.po.CourseMarket;
 import com.wyd.xuecheng.content.service.CourseBaseInfoService;
-import com.wyd.xuecheng.base.model.PageParams;
-import com.wyd.xuecheng.base.model.PageResult;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -75,31 +76,31 @@ public class CourseBaseInfoServiceImpl  implements CourseBaseInfoService {
     public CourseBaseInfoDto createCourseBase(Long companyId, AddCourseDto dto) {
         //合法性校验
         if (StringUtils.isBlank(dto.getName())) {
-            throw new RuntimeException("课程名称为空");
+            throw new XueChengPlusException("课程名称为空");
         }
 
         if (StringUtils.isBlank(dto.getMt())) {
-            throw new RuntimeException("课程分类为空");
+            throw new XueChengPlusException("课程分类为空");
         }
 
         if (StringUtils.isBlank(dto.getSt())) {
-            throw new RuntimeException("课程分类为空");
+            throw new XueChengPlusException("课程分类为空");
         }
 
         if (StringUtils.isBlank(dto.getGrade())) {
-            throw new RuntimeException("课程等级为空");
+            throw new XueChengPlusException("课程等级为空");
         }
 
         if (StringUtils.isBlank(dto.getTeachmode())) {
-            throw new RuntimeException("教育模式为空");
+            throw new XueChengPlusException("教育模式为空");
         }
 
         if (StringUtils.isBlank(dto.getUsers())) {
-            throw new RuntimeException("适应人群为空");
+            throw new XueChengPlusException("适应人群");
         }
 
         if (StringUtils.isBlank(dto.getCharge())) {
-            throw new RuntimeException("收费规则为空");
+            throw new XueChengPlusException("收费规则为空");
         }
         //新增对象
         CourseBase courseBaseNew = new CourseBase();
@@ -142,7 +143,7 @@ public class CourseBaseInfoServiceImpl  implements CourseBaseInfoService {
         //收费规则为收费
         if(charge.equals("201001")){
             if(courseMarketNew.getPrice() == null || courseMarketNew.getPrice() <=0){
-                throw new RuntimeException("课程为收费价格不能为空且必须大于0");
+                throw new XueChengPlusException("课程的价格不能为空并且必须大于0");
             }
         }
         //根据id从课程营销表查询
