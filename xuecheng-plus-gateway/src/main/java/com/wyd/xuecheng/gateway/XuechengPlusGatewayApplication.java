8b7b6240440b98519c3261bff5cfc36a9a6bcd0f
Stone-WYD <1174872613@qq.com>
2024-04-03 21:07:49 +0800

    网关项目

---
+++ xuecheng-plus-gateway/src/main/java/com/wyd/xuecheng/gateway/XuechengPlusGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.wyd.xuecheng.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuechengPlusGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuechengPlusGatewayApplication.class, args);
+    }
+
+}
