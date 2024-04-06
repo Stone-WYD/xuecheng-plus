92bb353a931b6793d563f4a9e83c7d6d215717ca
Stone-WYD <1174872613@qq.com>
2024-04-06 21:22:21 +0800

    提交媒资项目内容

---
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/wyd/xuecheng/media/mapper/MediaProcessHistoryMapper.java
@@ -0,0 +1,15 @@
+package com.wyd.xuecheng.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wyd.xuecheng.media.model.po.MediaProcessHistory;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MediaProcessHistoryMapper extends BaseMapper<MediaProcessHistory> {
+
+}
---
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/wyd/xuecheng/media/mapper/MqMessageMapper.java
@@ -0,0 +1,15 @@
+package com.wyd.xuecheng.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wyd.xuecheng.media.model.po.MqMessage;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MqMessageMapper extends BaseMapper<MqMessage> {
+
+}
