f65344b12e3d1194453089b32ba4eb687d12c049
Stone-WYD <1174872613@qq.com>
2024-04-07 16:25:30 +0800

    文件上传

---
+++ xuecheng-plus-media/xuecheng-plus-media-model/src/main/java/com/wyd/xuecheng/media/model/dto/UploadFileResultDto.java
@@ -0,0 +1,13 @@
+package com.wyd.xuecheng.media.model.dto;
+
+import com.wyd.xuecheng.media.model.po.MediaFiles;
+import lombok.Data;
+
+/**
+ * @program: xuecheng-plus-media
+ * @author: Stone
+ * @create: 2024-04-07 10:28
+ */
+@Data
+public class UploadFileResultDto extends MediaFiles {
+}
---
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/wyd/xuecheng/media/config/MinioConfig.java
@@ -0,0 +1,31 @@
+package com.wyd.xuecheng.media.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @program: xuecheng-plus-media
+ * @author: Stone
+ * @create: 2024-04-07 10:10
+ */
+@Configuration
+public class MinioConfig {
+
+    @Value("${minio.endpoint}")
+    private String endpoint;
+    @Value("${minio.accessKey}")
+    private String accessKey;
+    @Value("${minio.secretKey}")
+    private String secretKey;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+
+}
