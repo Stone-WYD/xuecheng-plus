d13fa2c9730c4955645d67fbbbf308a300769850
Stone-WYD <1174872613@qq.com>
2024-10-17 09:33:47 +0800

    调整授权模块配置类

--- xuecheng-plus-auth/src/main/java/com/wyd/xuecheng/auth/config/AuthorizationServer.java
+++ xuecheng-plus-auth/src/main/java/com/wyd/xuecheng/auth/config/AuthorizationServer.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -17,58 +18,79 @@ import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import javax.annotation.Resource;
 
 /**
- * @description 授权服务器配置
  * @author Mr.M
- * @date 2022/9/26 22:25
  * @version 1.0
+ * @description 授权服务器配置
+ * @date 2022/9/26 22:25
  */
- @Configuration
- @EnableAuthorizationServer
- public class AuthorizationServer extends AuthorizationServerConfigurerAdapter {
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServer extends AuthorizationServerConfigurerAdapter {
 
-  @Resource(name="authorizationServerTokenServicesCustom")
-  private AuthorizationServerTokenServices authorizationServerTokenServices;
+    @Resource(name = "authorizationServerTokenServicesCustom")
+    private AuthorizationServerTokenServices authorizationServerTokenServices;
 
- @Autowired
- private AuthenticationManager authenticationManager;
+    @Autowired
+    private WebSecurityConfig webSecurityConfig;
 
-  //客户端详情服务
-  @Override
-  public void configure(ClientDetailsServiceConfigurer clients)
-          throws Exception {
+    //客户端详情服务
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients)
+            throws Exception {
         clients.inMemory()// 使用in-memory存储
                 .withClient("XcWebApp")// client_id
 //                .secret("XcWebApp")//客户端密钥
                 .secret(new BCryptPasswordEncoder().encode("XcWebApp"))//客户端密钥（数据库密码要加密的话，这里也要加密，保持一致）
                 .resourceIds("xuecheng-plus")//资源列表
-                .authorizedGrantTypes("authorization_code", "password","client_credentials","implicit","refresh_token")// 该client允许的授权类型authorization_code,password,refresh_token,implicit,client_credentials
+                .authorizedGrantTypes("authorization_code", "password", "client_credentials", "implicit", "refresh_token")// 该client允许的授权类型authorization_code,password,refresh_token,implicit,client_credentials
                 .scopes("all")// 允许的授权范围
                 .autoApprove(false)//false跳转到授权页面
                 //客户端接收授权码的重定向地址
                 .redirectUris("http://www.51xuecheng.cn")
-   ;
-  }
+        ;
+    }
+
+    //令牌端点的访问配置
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        AuthenticationManager manager = webSecurityConfig.authenticationManagerBean();
+        endpoints.authenticationManager(manager)//认证管理器
+                .tokenServices(authorizationServerTokenServices)//令牌管理服务
+                .allowedTokenEndpointRequestMethods(HttpMethod.POST, HttpMethod.GET);// token 获取方法只能为设置的类型
+    }
+
+    //令牌端点的安全配置
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) {
+        security
+                .tokenKeyAccess("permitAll()")                    //oauth/token_key是公开
+                .checkTokenAccess("permitAll()")                  //oauth/check_token公开
+                .allowFormAuthenticationForClients()                //表单认证（申请令牌）
+        ;
+    }
 
 
-  //令牌端点的访问配置
-  @Override
-  public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-   endpoints
-           .authenticationManager(authenticationManager)//认证管理器
-           .tokenServices(authorizationServerTokenServices)//令牌管理服务
-           .allowedTokenEndpointRequestMethods(HttpMethod.POST, HttpMethod.GET);// token 获取方法只能为设置的类型
-  }
-
-  //令牌端点的安全配置
-  @Override
-  public void configure(AuthorizationServerSecurityConfigurer security){
-   security
-           .tokenKeyAccess("permitAll()")                    //oauth/token_key是公开
-           .checkTokenAccess("permitAll()")                  //oauth/check_token公开
-           .allowFormAuthenticationForClients()				//表单认证（申请令牌）
-   ;
-  }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+//        //密码为明文方式
+//        return NoOpPasswordEncoder.getInstance();
+        // 不管前端传入的密码是什么，存放时都会进行一层加密，这样做能更好的提高系统安全性，保护用户信息。
+        return new BCryptPasswordEncoder();
+    }
 
 
 
- }
+ /*public static void main(String[] args) {
+        String password = "111111";
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        for(int i=0;i<10;i++) {
+            //每个计算出的Hash值都不一样
+            String hashPass = passwordEncoder.encode(password);
+            System.out.println(hashPass);
+            //虽然每次计算的密码Hash值不一样但是校验是通过的
+            boolean f = passwordEncoder.matches(password, hashPass);
+            System.out.println(f);
+        }
+    }*/
+
+}
--- xuecheng-plus-auth/src/main/java/com/wyd/xuecheng/auth/config/WebSecurityConfig.java
+++ xuecheng-plus-auth/src/main/java/com/wyd/xuecheng/auth/config/WebSecurityConfig.java
@@ -20,58 +20,16 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
  * @date 2022/9/26 20:53
  */
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-
-/*    //配置用户信息服务
-    @Bean
-    public UserDetailsService userDetailsService() {
-        //这里配置用户信息,这里暂时使用这种方式将用户存储在内存中
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("zhangsan").password("123").authorities("p1").build());
-        manager.createUser(User.withUsername("lisi").password("456").authorities("p2").build());
-        return manager;
-    }*/
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-//        //密码为明文方式
-//        return NoOpPasswordEncoder.getInstance();
-        // 不管前端传入的密码是什么，存放时都会进行一层加密，这样做能更好的提高系统安全性，保护用户信息。
-        return new BCryptPasswordEncoder();
-    }
-
-        /*public static void main(String[] args) {
-        String password = "111111";
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        for(int i=0;i<10;i++) {
-            //每个计算出的Hash值都不一样
-            String hashPass = passwordEncoder.encode(password);
-            System.out.println(hashPass);
-            //虽然每次计算的密码Hash值不一样但是校验是通过的
-            boolean f = passwordEncoder.matches(password, hashPass);
-            System.out.println(f);
-        }
-    }*/
 
     //配置安全拦截机制
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/r/**").authenticated()//访问/r开始的请求需要认证通过
+        http.authorizeRequests().antMatchers("/r/**").authenticated()//访问/r开始的请求需要认证通过
                 .anyRequest().permitAll()//其它请求全部放行
-                .and()
-                .formLogin().successForwardUrl("/login-success") //登录成功跳转到/login-success
-                .and()
-                .logout().logoutUrl("/logout");//退出登录跳转到 /logout
-    }
-
-
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+                .and().formLogin().successForwardUrl("/login-success") //登录成功跳转到/login-success
+                .and().logout().logoutUrl("/logout");//退出登录跳转到 /logout
     }
 
 }
