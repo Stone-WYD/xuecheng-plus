6ca490876dc9c91aa78db8cb3ee669570de69135
Stone-WYD <1174872613@qq.com>
2024-11-15 10:34:34 +0800

    加入订单服务

---
+++ xuecheng-plus-orders/xuecheng-plus-orders-service/src/main/java/com/wyd/xuecheng/orders/mapper/XcPayRecordMapper.java
@@ -0,0 +1,15 @@
+package com.wyd.xuecheng.orders.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wyd.xuecheng.orders.model.po.XcPayRecord;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface XcPayRecordMapper extends BaseMapper<XcPayRecord> {
+
+}
